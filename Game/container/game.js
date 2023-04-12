43b4da4d84e9eed8219a9fa69b16b7bb8f5e4dcb
Rajesh M Y <rajumy123@gmail.com>
2023-04-12 17:40:42 +0530

    commit2

--- Game/container/game.js
+++ Game/container/game.js
@@ -1,6 +1,13 @@
 import React from "react";
-import { View, TouchableOpacity, Text, ScrollView } from "react-native";
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  ScrollView,
+  ToastAndroid,
+} from "react-native";
 import data from "../static/data.json";
+import Toast from "react-native-toast-message";
 
 let Game = () => {
   let b = [
@@ -15,62 +22,81 @@ let Game = () => {
       status: "AVAILABLE",
     },
   ];
+
+  const showToast = (e) => {
+    // Toast.show({
+    //   type: "success",
+    //   text1: e,
+    //   text2: "Pressed above game!!",
+    //   position: "bottom",
+    // });
+
+    ToastAndroid.show(
+      "You pressed: " + e,
+      ToastAndroid.SHORT,
+
+      ToastAndroid.TOP
+    );
+  };
+
   return (
-    <ScrollView
-      style={{
-        flex: 1,
-        backgroundColor: "black",
-        width: "100%",
-      }}
-    >
-      <View>
-        <Text
-          style={{
-            color: "#FFF",
-            fontSize: 20,
-            padding: 10,
-            justifyContent: "center",
-            alignContent: "center",
-            marginHorizontal: "30%",
-            marginVertical: 20,
-          }}
-        >
-          Games List!!
-        </Text>
-      </View>
-      <View style={{ flex: 1 }}>
-        {data.map((e) => {
-          return (
-            <View key={e.id}>
-              <TouchableOpacity
-                onPress={() => {
-                  window.alert("you pressed : " + e.title);
-                }}
-              >
-                <View
-                  style={{
-                    borderColor: "white",
-                    borderWidth: 1,
-                    borderRadius: 20,
-                    margin: 20,
-                    padding: 20,
-                    marginHorizontal: 40,
-                    backgroundColor: "grey",
-                    width: "80%",
-                    justifyContent: "center",
+    <View style={{ flex: 1 }}>
+      <ScrollView
+        style={{
+          flex: 1,
+          backgroundColor: "black",
+          width: "100%",
+        }}
+      >
+        <View>
+          <Text
+            style={{
+              color: "#FFF",
+              fontSize: 20,
+              padding: 10,
+              justifyContent: "center",
+              alignContent: "center",
+              marginHorizontal: "30%",
+              marginVertical: 20,
+            }}
+          >
+            Games List!!
+          </Text>
+        </View>
+        <View style={{ flex: 1 }}>
+          {data.map((e) => {
+            return (
+              <View key={e.id}>
+                <TouchableOpacity
+                  onPress={() => {
+                    showToast(e.title);
                   }}
                 >
-                  <Text style={{ color: "white" }}>{e.title}</Text>
-                  <Text style={{ color: "white" }}>{e.publisher}</Text>
-                  <Text style={{ color: "white" }}>{e.genre}</Text>
-                  <Text style={{ color: "white" }}>{e.status}</Text>
-                </View>
-              </TouchableOpacity>
-            </View>
-          );
-        })}
-      </View>
-    </ScrollView>
+                  <View
+                    style={{
+                      borderColor: "white",
+                      borderWidth: 1,
+                      borderRadius: 20,
+                      margin: 20,
+                      padding: 20,
+                      marginHorizontal: 40,
+                      backgroundColor: "grey",
+                      width: "80%",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Text style={{ color: "white" }}>{e.title}</Text>
+                    <Text style={{ color: "white" }}>{e.publisher}</Text>
+                    <Text style={{ color: "white" }}>{e.genre}</Text>
+                    <Text style={{ color: "white" }}>{e.status}</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            );
+          })}
+        </View>
+      </ScrollView>
+    </View>
   );
 };
 
